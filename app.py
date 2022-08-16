8bc280769534954f1c51b7541b62e6750bf4ffa6
Terry Roberts <terryjbroberts@yahoo.com>
2022-08-15 22:08:49 -0500

    refactored the positions of my classes and schemas

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+
 import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -14,40 +16,17 @@ bcrypt = Bcrypt(app)
 CORS(app)
 
 class User(db.Model):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
     meal = db.relationship('Food', backref='user', cascade='all, delete, delete-orphan')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-@app.route('/user/add', methods=['POST'])
-def add_user():
-    post_data = request.get_json()
-    username = post_data.get('username')
-    password = post_data.get('password')
-    possible_dup = db.session.query(User).filter(User.username == username).first()
-    
-    if possible_dup is not None:
-        return jsonify('Error: that username already exists. Please choose another one.') 
-
-    encrypted_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    new_user = User(username, encrypted_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify('You have created a new user. Welcome to the site!')
-
-@app.route('/user/get/<id>', methods=['GET'])
-def get_user(id):
-    user = db.session.query(User).filter(User.id == id).first()
-    return jsonify(user_schema.dump(user))
 
 class Food(db.Model):
-    __tablename__ = 'food'
     id = db.Column(db.Integer, primary_key=True)
     menu_type = db.Column(db.String, nullable=False)
     title = db.Column(db.String, nullable=False)
@@ -62,7 +41,7 @@ class Food(db.Model):
 
 class FoodSchema(ma.Schema):
     class Meta:
-        fields = ("id", "title", "price", "menu_type",'user_fk')
+        fields = ("id", "title", "price", "menu_type", "user_fk")
 
 food_schema = FoodSchema()
 multiple_food_schema = FoodSchema(many=True)
@@ -74,6 +53,8 @@ class UserSchema(ma.Schema):
 
 user_schema = UserSchema()
 multiple_user_schema = UserSchema(many=True)
+
+# Food Endpoints
 
 @app.route("/food/add", methods=["POST"])
 def add_food():
@@ -98,6 +79,31 @@ def get_all_food():
 def get_items_by_type(menu_type):
     records = db.session.query(Food).filter(Food.menu_type == menu_type).all()
     return jsonify(multiple_food_schema.dump(records))
+
+# User Endpoints
+
+@app.route('/user/add', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    username = post_data.get('username')
+    password = post_data.get('password')
+    user = db.session.query(User).filter(User.username == username).first()
+
+    if user is not None:
+        return jsonify('Error: You must use another name. That one is taken!')
+
+    encrypted_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username, encrypted_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify('You have created a user! Welcome to our site!')
+
+
+@app.route('/user/get/<id>', methods=['GET'])
+def get_user(id):
+    user = db.session.query(User).filter(User.id == id).first()
+    return jsonify(user_schema.dump(user))
 
 if __name__ == "__main__":
     app.run(debug=True)
